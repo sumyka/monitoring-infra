b08f74e31a0c0e95a58ec14287851fe90ea68c75
sumyka@RaspberryPi <206566026+sumyka@users.noreply.github.com>
2026-05-27 16:02:14 +0900

    feat: 常に温湿度を取得し続けるように変更

--- read_dht11.py
+++ read_dht11.py
@@ -1,12 +1,15 @@
+import time
 import board
 import adafruit_dht
 
 dht = adafruit_dht.DHT11(board.D17) # GPIOのピン番号を指定
 
 try:
-    temp = dht.temperature
-    hum = dht.humidity
+    while True:
+        temp = dht.temperature
+        hum = dht.humidity
 
-    print(f"Temperature: {temp}°C, Humidity: {hum}%")
+        print(f"Temperature: {temp}°C, Humidity: {hum}%")
+        time.sleep(30)
 finally:
     dht.exit()
